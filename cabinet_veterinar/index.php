266ac8473d23dd66587444af78936e55fa7822fc
Radu3121 <80837837+Radu3121@users.noreply.github.com>
2021-05-17 16:51:11 +0300

    Delete cabinet_veterinar directory

--- cabinet_veterinar/index.php
+++
@@ -1,5 +0,0 @@
-<!DOCTYPE html>
-<html>
-    <!-- pagina de pornire este cea de login, index-ul face redirect automat catre ea -->
-    <meta http-equiv = "refresh" content = "0; url = http://localhost/cabinet_veterinar/login.php" />
-</html>
--- cabinet_veterinar/iudFooterPacienti.php
+++
@@ -1,7 +0,0 @@
-    <table class = 'iud'>
-        <tr>
-            <td><a href ="insertPacienti.php"><button class = "iudButton">Insert</button></a></td>
-            <td><a href ="updatePacienti.php"><button class = "iudButton">Update</button></a></td>
-            <td><a href ="deletePacienti.php"><button class = "iudButton">Delete</button></a></td>
-        </tr>
-    </table>
